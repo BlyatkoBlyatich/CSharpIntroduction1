eb08d05fae28ab846d2669809945d87140d6b817
Василий Виссаров <vissar2016@rambler.ru>
2022-10-14 00:23:08 +0300

    HomeWork 1 is done!

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿/*Задача № 2
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.*/
+
+/*
+Console.Write("Input first number "); // Ввод первого числа
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number "); // Ввод второго числа
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2) 
+{
+    Console.WriteLine(num1 + " is largest then " + num2);
+}
+    else
+    {
+        Console.WriteLine(num2 + " is largest then " + num1);
+    }
+    
+    */
+
+//  Задача №4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input first number "); // Ввод первого числа
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number "); // Ввод второго числа
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a third number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2 && num1>num3)
+{
+    Console.WriteLine($"The most largest number is {num1}");
+}
+if (num2>num1 && num2>num3)
+{
+    Console.WriteLine($"The most largest number is {num2}");
+}
+if (num3>num2 && num3>num1);
+{
+    Console.WriteLine($"The most largest number is {num3}");
+}
+*/
+
+/* Задача №6*Напишите программу, которая на вход принимает число и выдаёт, является ли 
+число чётным (делится ли оно на два без остатка).*/
+/*
+Console.Write("Input a positive natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2==0)
+{
+    Console.WriteLine($"Число {number} является четным.");
+}
+else
+{
+    Console.WriteLine($"Число {number} не является четным.");
+}
+*/
+/* Задача №8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.WriteLine("Input integer number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count_start = 2;
+
+while(count_start<=num)
+{
+Console.Write(count_start + " ");
+count_start = count_start + 2;  
+}
+*/
